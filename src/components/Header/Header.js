cec555dfd0fde89b7d14e5b58299eec20818efd4
Michael Kostal <kostal@gmail.com>
2024-02-04 13:22:06 -0600

    Ex1: Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ import Button from '../Button';
 
 const Header = () => {
   return (
-    <header>
+    <Wrapper>
       <SuperHeader>
         <Row>
           <ActionGroup>
@@ -29,16 +29,35 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <DesktopActionGroup>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </DesktopActionGroup>        
         <Logo />
+        <SubscribeWrapper>
+          <Button>Subscribe</Button>
+          <SubLink href="/">Already a member?</SubLink>
+        </SubscribeWrapper>
       </MainHeader>
-    </header>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+
+`;
 
 const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+  @media(${QUERIES.laptopAndUp}){
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -59,12 +78,52 @@ const ActionGroup = styled.div`
   }
 `;
 
+const DesktopActionGroup = styled(ActionGroup)`
+  display: none;
+  @media(${QUERIES.laptopAndUp}){
+    display: flex;
+  }
+`;
+
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+  @media(${QUERIES.tabletAndUp}){
+    margin-top: 48px;
+    margin-bottom: 72px;
+  }
+  @media(${QUERIES.laptopAndUp}){
+    display: grid;
+    grid-template-columns: 1fr auto 1fr;
+    align-items: center;
+    justify-content: start;
+    margin-top: 16px;
+  }
+`;
+
+const SubscribeWrapper = styled.div`
+  display: none;
+  @media(${QUERIES.laptopAndUp}){
+    justify-self: end;
+    align-self: center;
+    display: block;
+    position: relative;
+  }
+`
+
+const SubLink = styled.a`
+  position: absolute;
+  margin-top:8px;
+  display:block; 
+  width: 100%;
+  text-align: center;
+  color: var(--color-gray-900);
+  font-style:italic;
+  text-decoration: underline;
+  font-size:#{14/16}rem;
 `;
 
 export default Header;
